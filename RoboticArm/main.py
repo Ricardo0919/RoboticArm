c61146223961c9493f8eac9bde68213e899e043e
Ricardo0919 <rickisierra03@gmail.com>
2024-06-10 12:40:40 -0600

    update

--- RoboticArm/main.py
+++ RoboticArm/main.py
@@ -1,36 +1,33 @@
 import serial
-from fastapi import FastAPI, Request, WebSocket
+from fastapi import FastAPI, Request
 from fastapi.templating import Jinja2Templates
 from fastapi.responses import HTMLResponse
 
 app = FastAPI()
-templates = Jinja2Templates(directory=".")
+templates = Jinja2Templates(directory="templates")  # Correct directory path
 
 # Abrir la conexión serial
-#ser = serial.Serial('/dev/ttyACM0', 9600)  # Modifica el puerto y la velocidad según tus necesidades
+ser = serial.Serial('/dev/ttyACM0', 9600)  # Modifica el puerto y la velocidad según tus necesidades
 
 # Función para leer datos del puerto serial
 def read_serial_data():
-    # Lee los datos del puerto serial
-    #data = ser.readline().decode().strip()  # Decodifica y elimina espacios en blanco
-    data = 0
+    data = ser.readline().decode().strip()  # Decodifica y elimina espacios en blanco
+    print(data)
     return data
 
 # Ruta para la página principal
 @app.get("/", response_class=HTMLResponse)
 async def main(request: Request):
-    # Renderizar el HTML contenido en este mismo archivo
     return templates.TemplateResponse("index.html", {"request": request})
 
-# Ruta WebSocket para enviar datos del puerto serial al cliente
-@app.websocket("/ws")
-async def websocket_endpoint(websocket: WebSocket):
-    await websocket.accept()
-    while True:
-        # Leer datos del puerto serial
-        data = read_serial_data()
-        if data:
-            await websocket.send_text(data)
+# Ruta para obtener los datos seriales
+
+@app.get("/serial_data.html", response_class=HTMLResponse)
+async def get_serial_data(request: Request):
+    data = read_serial_data()
+    return templates.TemplateResponse("serial_data.html", {"request": request, "data": data})
+
+
 
 if __name__ == '__main__':
     import uvicorn
